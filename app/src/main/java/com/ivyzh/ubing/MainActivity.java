531ce9609aec17d770316cff6613cd2a3e23375a
Ivyzh <app_do@163.com>
2018-12-14 00:33:46 +0800

    Initial commit

---
+++ app/src/main/java/com/ivyzh/ubing/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ivyzh.ubing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
